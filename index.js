2c1be4b98eb8928d6661d352b00fafdde194fe63
Shilpi Verma <shilpiverma509@gmail.com>
2018-03-30 15:28:01 -0500

    modified engine in package.json

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = express();
 
-//route-handler
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
 });
 
-const PORT = process.env.port || 5000;
-app.listen(PORT); //express tells node to listen to port 5000
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
